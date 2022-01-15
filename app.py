56823f0d23eadd35cd01cbc5ea0e5a2be078e6e1
chao by mac <zhilan.cc@gmail.com>
2022-01-15 17:42:49 +0800

    调整二维码生成器，新增自定义Logo

--- app.py
+++ app.py
@@ -90,4 +90,4 @@ def random_filename(filename):
     return new_filename
 
 if __name__ == '__main__':
-    app.run(debug=True, host="192.168.33.10", port=8084)
+    app.run(debug=True, host="127.0.0.1", port=8084)
