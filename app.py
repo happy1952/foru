b346f4a25a6c8ac1a3e7548dc6bf7f623e4c57d2
chao by mac <zhilan.cc@gmail.com>
2022-01-16 20:04:18 +0800

    优化图片裁剪

--- app.py
+++ app.py
@@ -58,23 +58,40 @@ def upload():
     output_filename = output_dir + filename
     img = cv2.imread(input_filename, cv2.IMREAD_COLOR)
 
-    # 提取图片中的红色区域
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    ## 1. 转换为灰度和阈值
+    ## (1) Convert to gray, and threshold
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    th, threshed = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
 
+    ## 2. Morph-op 去除噪音
+    ## (2) Morph-op to remove noise
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
+    morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
+
+    ## 3. 找到最大面积轮廓
+    ## (3) Find the max-area contour
+    cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    cnt = sorted(cnts, key=cv2.contourArea)[-1]
+
+    ## 4. 裁剪并保存
+    ## (4) Crop and save it
+    x,y,w,h = cv2.boundingRect(cnt)
+    dst = img[y:y+h, x:x+w]
+
+    # 提取图片中的红色区域
+    hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
     lower_red = np.array([0,70,50])
     upper_red = np.array([10,255,255])
     mask1 = cv2.inRange(hsv, lower_red, upper_red)
-
     lower_red = np.array([170,70,50])
     upper_red = np.array([180,255,255])
     mask2 = cv2.inRange(hsv, lower_red, upper_red)
-
     mask = cv2.add(mask1, mask2)
 
-    output_img = mask.copy()
-    output_img[np.where(mask==0)] = 0
-
-    output_hsv = img.copy()
+    # 背景透明
+    # output_img = mask.copy()
+    # output_img[np.where(mask==0)] = 0
+    output_hsv = dst.copy()
     output_hsv = cv2.cvtColor(output_hsv, cv2.COLOR_BGR2BGRA)
     output_hsv[np.where(mask==0)] = [255,255,255,0]
     cv2.imwrite(output_filename, output_hsv)
